1e7f675f437901d069484a82a94b71368a032d70
Hacker-2004 <117286808+Hacker-2004@users.noreply.github.com>
2025-03-20 08:58:58 -0400

    add the components to home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoadingScreen from './pages/loadingScreen';
+
+// Lazy load the Home and About components
+const Home = lazy(() => import('./pages/home'));
+//const About = lazy(() => import('./pages/About'));
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 4300);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      {loading ? (
+        <LoadingScreen />
+      ) : (
+          <Routes>
+          <Route path="/" element={<Home />} />
+          </Routes>
+      )}
+    </Router>
+  );
 }
 
-export default App
+export default App;
