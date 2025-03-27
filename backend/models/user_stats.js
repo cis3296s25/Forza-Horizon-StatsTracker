06cf3d820c59dda7108bbe4c8e50ea69798891bf
Hacker-2004 <117286808+Hacker-2004@users.noreply.github.com>
2025-03-27 11:06:36 -0400

    add stats by meet

--- backend/models/user_stats.js
+++ backend/models/user_stats.js
@@ -5,6 +5,50 @@ const statsSchema = new mongoose.Schema({
      type: String,
      required: true
     },
+    timeDriven:{
+        type: String,
+        required: true
+       },
+    numberofCarsOwned:{
+        type: Number,
+        required: true
+       },
+    mostValuableCar:{
+        type: String,
+        required: true
+       },
+    totalWinnningsinCR:{
+        type: Number,
+        required: true
+       },
+    favoriteCar:{
+        type: String,
+        required: true
+       },
+    garageValue:{
+        type: Number,
+        required: true
+       },
+    longestSkillChain:{
+        type: String,
+        required: true
+       },
+    distanceDrivenInMiles:{
+        type: Number,
+        required: true
+       },
+    longestJump:{
+        type: Number,
+        required: true
+       },
+    topSpeed:{
+        type: Number,
+        required: true
+       },
+    biggestAir:{
+        type: String,
+        required: true
+       }
 })
 
 module.exports = mongoose.model('user_stats', statsSchema);
