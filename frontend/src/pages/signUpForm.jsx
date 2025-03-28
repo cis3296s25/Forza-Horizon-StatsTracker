e53ec78b5e59fb2e4fd7b9c3c9af54337f4b4c63
Hacker-2004 <117286808+Hacker-2004@users.noreply.github.com>
2025-03-28 19:37:44 -0400

    security-patch-1.1

--- frontend/src/pages/signUpForm.jsx
+++ frontend/src/pages/signUpForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'; // Import useEffect here
 import "../styles/signup.css";
 import Nav from '../components/nav';
 import Footer from '../components/footer';
