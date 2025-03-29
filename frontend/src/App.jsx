79f16b8304a0979a7398b9478cfc40aaf397bb7e
Hacker-2004 <117286808+Hacker-2004@users.noreply.github.com>
2025-03-28 20:11:19 -0400

    security-patch-1.3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Toaster } from "react-hot-toast";
 import LoadingScreen from './pages/loadingScreen';
 
-// Lazy load the components
 const Home = lazy(() => import('./pages/home'));
 const Profile = lazy(() => import('./pages/profile'));
 const Signup = lazy(() => import('./pages/signup'));
 const StatsPage = lazy(() => import('./pages/statsPage'));
 const SignupForm = lazy(() => import('./pages/signUpForm'));
-const NotFound = lazy(() => import('./components/notFound'));
+const NotFound = lazy(() => import('./components/NotFound'));
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -27,15 +26,16 @@ function App() {
       {loading ? (
         <LoadingScreen />
       ) : (
+        <Suspense>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/signup-stats" element={<SignupForm />} />
             <Route path="/user/:username" element={<StatsPage />} />
-            
             <Route path="*" element={<NotFound />} />
           </Routes>
+        </Suspense>
       )}
       <Toaster position="bottom-center" />
     </Router>
