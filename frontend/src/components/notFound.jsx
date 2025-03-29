c5bd12f03fa59ae1a9d14041e9f73b4ecd1c80f3
Hacker-2004 <117286808+Hacker-2004@users.noreply.github.com>
2025-03-28 20:07:16 -0400

    security-patch-1.3

--- frontend/src/components/notFound.jsx
+++ frontend/src/components/notFound.jsx
@@ -1,35 +1,36 @@
-import React from 'react'
+// components/NotFound.js
+import React from 'react';
 import Nav from './nav';
 import Footer from './footer';
 
-const notFound = () => {
+const NotFound = () => {
   return (
     <div>
-      <Nav/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <h1 style={{textAlign:"center"}}>404 - Page Not Found</h1>
-      <h5 style={{textAlign:"center"}}>The page you are looking for doesn't exist.</h5>
-      <h5 style={{textAlign:"center"}}>Click on the logo to go to home page</h5>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-    <Footer />
+      <Nav />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1 style={{ textAlign: "center" }}>404 - Page Not Found</h1>
+      <h5 style={{ textAlign: "center" }}>The page you are looking for doesn't exist.</h5>
+      <h5 style={{ textAlign: "center" }}>Click on the logo to go to home page</h5>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default notFound
+export default NotFound;
