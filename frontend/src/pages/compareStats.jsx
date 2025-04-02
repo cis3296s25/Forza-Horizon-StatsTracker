d24ee85fbf7c515d6df4a188675e5191e74711d2
Victor Awoleye <tup40950@temple.edu>
2025-04-02 10:40:58 -0400

    added button and search box

--- frontend/src/pages/compareStats.jsx
+++ frontend/src/pages/compareStats.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import "../styles/CompareStats.css";
 import race from '../assets/forzaImgs/mclaren.jpg';
+import{ useState } from 'react';
 
 const CompareStats = () => {
+  const[userName, setUserName] = useState('');
+
+  const handleChange = (event) =>{
+    setUserName(event.target.value);
+  }
+
   return (
     
 <div
@@ -19,14 +26,24 @@ display: 'flex',
 justifyContent: 'flex-start',
 alignItems: 'flex-start',
 padding: '1rem'
-}} 
-    >   
+}} >   
 
-  <h1>
-    COMPARE STATS
-   </h1>
-  
-   </div>
+<div className = "contents-of-compare">
+  <h1>COMPARE STATS</h1>
+
+  <div className = "search-contents">
+<input 
+type ="text" 
+className = "username-input"
+placeholder="Enter a UserName to Compare With"
+onChange={handleChange}
+/>
+<button className = "compare-btn">COMPARE</button>
+    </div>
+</div>
+
+
+ </div>
   )
 }
 
